38d33be42f6ad8230ba5d25a2f234381ead7fc25
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-12 20:02:20 +0300

    add a dynamic route to the site

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app
+from . import app
 
 
 # Views
@@ -8,4 +8,13 @@ def index():
     """
     View root page function that returns the index page and its data
     """
-    return render_template('index.html')
+    message = 'Hello World'
+    return render_template('index.html', message=message)
+
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    """
+    View movie page function that returns the movie details page and its data
+    """
+    return render_template('movie.html', id=movie_id)
