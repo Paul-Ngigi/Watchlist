9168e6df4e822f2ab155a1341e935fdd09cd7422
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-21 08:50:42 +0300

    add the logic to connect to a db

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,8 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
-from ..requests import get_movies,get_movie,search_movie
+from ..requests import get_movies, get_movie, search_movie
 from .forms import ReviewForm
 from ..models import Review
-
 
 
 # Views
@@ -23,16 +22,16 @@ def index():
     # Getting now playing
     now_showing_movie = get_movies('now_playing')
     print(now_showing_movie)
-    
+
     title = 'Home - Welcome to The Best Movie Review Website Online'
-    
+
     search_movie = request.args.get('movie_query')
-    
+
     if search_movie:
         return redirect(url_for('search', movie_name=search_movie))
     else:
         return render_template('index.html', title=title, popular=popular_movie, upcoming=upcoming_movie,
-                           now_showing=now_showing_movie)
+                               now_showing=now_showing_movie)
 
 
 @main.route('/movie/<int:id>')
@@ -55,19 +54,20 @@ def search(movie_name):
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies)
+    return render_template('search.html', movies=searched_movies)
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET', 'POST'])
+
+@main.route('/movie/review/new/<int:id>', methods=['GET', 'POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
-    
+
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
+        new_review = Review(movie.id, title, movie.poster, review)
         new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
+        return redirect(url_for('movie', id=movie.id))
 
     title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+    return render_template('new_review.html', title=title, review_form=form, movie=movie)
