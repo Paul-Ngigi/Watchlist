376a963c162f92029b4846fe030e4ccc85894c02
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-22 10:44:49 +0300

    add login form logic

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -23,3 +23,9 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That username is taken')
         
         
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+    
